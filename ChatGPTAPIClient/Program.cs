21aa57908a8adace833bb3a848eb9876fda21c7b
WFPConsultant <nayeemjamil321@gmail.com>
2025-05-08 11:56:22 +0600

    Update Program.cs

--- ChatGPTAPIClient/Program.cs
+++ ChatGPTAPIClient/Program.cs
@@ -15,7 +15,7 @@ builder.Services.AddSingleton(Secrets);
 
 
 // Add services to the container.
-
+builder.Services.AddHttpClient();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
